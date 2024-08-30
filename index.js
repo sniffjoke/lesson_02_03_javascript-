6819c5f285e7a8b9b2521c4075cca2db8458c53e
sniffjoke <sniffjoke@gmail.com>
2024-08-30 17:40:39 +0200

    repeat old tasks

---
+++ index.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch'
+
+// const pr = fetch('https://google.com')
+//
+// pr.then((data) => {
+//     console.log('data from Google: ' , data.url)
+// })
+
+// fetch('https://googlejdkfsjalkfejwla.com')
+//     .then((data) => {
+//         console.log('data from google: ', data.url)
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     })
+
+// fetch('https://yahoo.com').then((data) => {
+//     console.log('data from yahoo: ' , data.url)
+//     return fetch('https://google.com')
+// }).then((data) => {
+//     console.log('data from google: ', data.url)
+//     return fetch('https://bingsak.com')
+// }).then((data) => {
+//     console.log('data from bing: ' , data.url)
+// }).catch(err => {
+//     console.log(err)
+// })
